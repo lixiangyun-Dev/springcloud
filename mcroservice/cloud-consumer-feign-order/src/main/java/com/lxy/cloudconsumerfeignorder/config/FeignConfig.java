1e5e104af0203157b7bfea31d687696bbc126d81
lixiangyun <575559167@qq.com>
2023-04-12 15:37:28 +0800

    springcloud项目--集成OpenFeign服务接口调用,Feign logger日志模块配置

--- mcroservice/cloud-consumer-feign-order/src/main/java/com/lxy/cloudconsumerfeignorder/config/FeignConfig.java
+++ mcroservice/cloud-consumer-feign-order/src/main/java/com/lxy/cloudconsumerfeignorder/config/FeignConfig.java
@@ -9,7 +9,12 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig {
     /**
      * feignClient配置日志级别
-     *
+     *Logger有四种类型：
+     * NONE：默认的，不显示任何日志。
+     * BASIC：仅记录请求方法、URL、响应状态及执行时间。
+     * HEADERS：除了BASIC中定义的信息之外，还有请求和响应的有信息。
+     * FULL：除了BASIC中定义的信息之外，还有请求和响应的正文及元数据。
+     * 通过注册Bean来设置日志记录级别！
      * @return
      */
     @Bean
